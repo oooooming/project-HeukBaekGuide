190a50adf47e451737431f32e7dc45d38cc9153b
henry726726 <msj3767@gmail.com>
2024-11-23 12:58:29 +0900

    user,favorite,search- domain

--- src/main/java/com/example/heukbaekguide/domain/Favorite.java
+++ src/main/java/com/example/heukbaekguide/domain/Favorite.java
@@ -1,4 +1,30 @@
 package com.example.heukbaekguide.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Favorite {
+
+    @Id
+    @Column(name = "favorite_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fileId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
 }
--- src/main/java/com/example/heukbaekguide/domain/Search.java
+++ src/main/java/com/example/heukbaekguide/domain/Search.java
@@ -1,4 +1,37 @@
 package com.example.heukbaekguide.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.dialect.function.TruncFunction;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Search {
+
+    @Id
+    @Column(name = "search_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fileId;
+
+    @Column(nullable = false)
+    private String search_term;
+
+    @Column(nullable = false)
+    private TruncFunction.DatetimeTrunc search_at;
+
+    @Column(nullable = false)
+    private String user_name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
 }
