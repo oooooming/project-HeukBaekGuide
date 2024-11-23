53d5a57a0f9c9b12ce0fec10dd6804027cb5c26f
JeonJaeeun <jane9518@naver.com>
2024-11-23 13:22:35 +0900

    다시 수정

--- src/main/frontend/src/components/RestaurantSlider.jsx
+++ src/main/frontend/src/components/RestaurantSlider.jsx
@@ -3,7 +3,7 @@ import "./RestaurantSlider.css";
 
 const RestaurantSlider = ({ showAll }) => {
     const dummyCards = [
-        { id: 1, title: "Restaurant A", description: "Delicious !" },
+        { id: 1, title: "Restaurant A", description: "Delicious !!" },
         { id: 2, title: "Restaurant B", description: "Delicious !" },
         { id: 3, title: "Restaurant C", description: "Delicious !" },
         { id: 4, title: "Restaurant D", description: "Delicious !" },
