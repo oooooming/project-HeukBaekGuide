5cc80d9d2a2c19d78f4bc2f607cb7aa49662b56f
oooooming <minjio030107@ewhain.net>
2024-11-23 12:54:37 +0900

    menuItem, restaurant, review, visit domain

--- src/main/java/com/example/heukbaekguide/domain/MenuItem.java
+++ src/main/java/com/example/heukbaekguide/domain/MenuItem.java
@@ -1,4 +1,36 @@
 package com.example.heukbaekguide.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MenuItem {
+
+    @Id
+    @Column(name = "menuItem_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int menuItemId;
+
+    @Column(name = "name", nullable = false)
+    private String menuItemName;
+
+    @Column(name = "description")
+    private String menuItemDescription;
+
+    @Column(name = "image")
+    private String menuItemImage;
+
+    @Column(name = "price")
+    private String menuItemPrice;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
 }
--- src/main/java/com/example/heukbaekguide/domain/Review.java
+++ src/main/java/com/example/heukbaekguide/domain/Review.java
@@ -1,4 +1,34 @@
 package com.example.heukbaekguide.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Review {
+
+    @Id
+    @Column(name = "review_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
 }
