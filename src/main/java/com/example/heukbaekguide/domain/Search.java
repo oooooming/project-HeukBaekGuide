1129412bcf0f9e8b000d8ff09f278ea76e368547
henry726726 <msj3767@gmail.com>
2025-01-06 21:49:56 +0900

    user/userrepsitory 수정

--- src/main/java/com/example/heukbaekguide/domain/Search.java
+++ src/main/java/com/example/heukbaekguide/domain/Search.java
@@ -3,7 +3,6 @@ package com.example.heukbaekguide.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.dialect.function.TruncFunction;
 
 import java.time.LocalDateTime;
 
--- src/main/java/com/example/heukbaekguide/repository/FavoriteRepository.java
+++ src/main/java/com/example/heukbaekguide/repository/FavoriteRepository.java
@@ -1,7 +1,6 @@
 package com.example.heukbaekguide.repository;
 
 import com.example.heukbaekguide.domain.Favorite;
-import com.example.heukbaekguide.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findByUser(User user);
+    List<Favorite> findByUser(Long user);
 }
